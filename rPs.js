f29aa403e2b9e1226e026aeb4c1b0987b87a0ef8
Mart510 <martynandrewbillings@gmail.com>
2021-08-22 13:55:35 +0100

    Initial set up commit

---
+++ rPs.js
@@ -0,0 +1,26 @@
+// Rock Paper Scissors in Javascript
+
+// Function called computerPlay that will return randomly 'Rock', 'Paper' or 'Scissors'
+function computerPlay(compChoice) {
+    const randval = array["Rock", "Paper", "Scissors"];
+    compChoice = randval(random);
+}
+
+
+/* Function called roundMaster to play a round of Rock Paper Scissors.
+
+Take the two parameters 'playerSelection' and 'computerSelection'
+
+Compare the two, find the result,
+
+Paper beats Rock
+Rock beats Scissors
+Scissors beats Paper
+
+and then return a string that decalares the winner with a victory message e.g. 'You Lose! Paper beats Rock'
+
+*/
+
+// Function playerSelection: takes player input and stores it as a string. using prompt()
+
+// Function called game() which runs roundMaster 5 times in a row, then displays the results of each round and the winner after 5 games using console.log()
